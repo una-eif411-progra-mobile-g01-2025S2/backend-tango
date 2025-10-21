4056fb3b3fbdc9929349848cf158c46217b84b2e
JerrySC68 <jerrysc68@gmail.com>
2025-10-20 21:47:00 -0600

    Cambios con JWT

---
+++ src/main/kotlin/cr/una/pai/domain/Service.kt
@@ -0,0 +1,61 @@
+package cr.una.pai.domain
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+@Transactional
+class AppUserDetailsService(
+    @Autowired
+    val userRepository: UserRepository,
+    @Autowired
+    val roleRepository: RoleRepository,
+) : UserDetailsService {
+
+    /**
+     * Locates the user based on the username. In the actual implementation, the search
+     * may be case sensitive, or case insensitive depending on how the
+     * implementation instance is configured. In this case, the `UserDetails`
+     * object that comes back may have a username that is of a different case than what
+     * was actually requested..
+     * @param username the username identifying the user whose data is required.
+     * @return a fully populated user record (never `null`)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     * GrantedAuthority
+     */
+    @Throws(UsernameNotFoundException::class)
+    override fun loadUserByUsername(username: String): UserDetails {
+        val userAuth: org.springframework.security.core.userdetails.User
+        val user: User = userRepository.findByEmail(username).orElse(null)
+            ?: return org.springframework.security.core.userdetails.User(
+                "", "", true, true, true, true,
+                getAuthorities(
+                    listOf(
+                        roleRepository.findByName("ROLE_USER").get()
+                    )
+                )
+            )
+
+        userAuth = org.springframework.security.core.userdetails.User(
+            user.email, user.password, user.enabled, true, true,
+            true, getAuthorities(user.roleList!!.toMutableList())
+        )
+
+        return userAuth
+    }
+
+    private fun getAuthorities(roles: Collection<Role>): Collection<GrantedAuthority> {
+        return roles.flatMap { role ->
+            sequenceOf(SimpleGrantedAuthority(role.name)) +
+                    role.privilegeList.map { privilege -> SimpleGrantedAuthority(privilege.name) }
+        }.toList()
+    }
+
+}
