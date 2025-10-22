421616bc939770ec2be689a367d276d9862b5a0f
josevalverdesolis-UNA <jose.valverde.solis@est.una.ac.cr>
2025-10-22 07:07:46 -0600

    a ver

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,23 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("jvm") version "1.9.25"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.25"
 	kotlin("plugin.jpa") version "1.9.25"
-	kotlin("kapt") version "1.9.25"
+	kotlin("kapt") version "1.9.24"
 }
 
 group = "backend-tango"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(17))
-	}
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+	jvmToolchain(17)
 }
 
 repositories {
